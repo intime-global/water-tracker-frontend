ffbd5d34d5d973342f81ef34ae09075a03f6255e
Serhii Reshetniak <reshet@lozovaya.org.ua>
2024-12-14 21:44:41 +0200

    Remove Suspense

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,15 +1,11 @@
 import css from './SharedLayout.module.css';
 import Header from '../Header/Header.jsx';
-import Loader from '../Loader/Loader.jsx';
-import { Suspense } from 'react';
 
 export default function SharedLayout({ children }) {
   return (
     <div className={css.layoutContainer}>
       <Header />
-      <main>
-        <Suspense fallback={Loader}>{children}</Suspense>
-      </main>
+      <main>{children}</main>
     </div>
   );
 }

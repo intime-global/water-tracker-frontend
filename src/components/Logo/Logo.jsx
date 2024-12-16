710f118763f17113d66b2800edeb71f477242ada
AnastasiiaPerevytska <anastasiia.zelenina.1995@gmail.com>
2024-12-16 17:59:32 +0200

    add  logic for  header & logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,11 +1,15 @@
 import css from './Logo.module.css';
 import { NavLink } from 'react-router-dom';
 import LogoSvg from '../../icons/logo.svg?react';
+import { useSelector } from 'react-redux';
+import { selectIsLoggedIn } from '../../redux/user/selectors.js';
 
 const Logo = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
   return (
     <nav className={css.nav}>
-      <NavLink className={css.logoLink} to="/welcome">
+      <NavLink className={css.logoLink} to={isLoggedIn ? '/home' : '/welcome'}>
         <LogoSvg className={css.logoIcon} />
       </NavLink>
     </nav>

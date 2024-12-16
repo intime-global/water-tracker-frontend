553ef10212c64ed1259404747c97e97ddbeac0d4
Victoria <vika96karnauhova@gmail.com>
2024-12-16 14:35:16 +0200

    changes in welcome

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,11 +2,13 @@
 // import WaterRatioPanel from '../../components/WaterRatioPanel/WaterRatioPanel';
 import TodayWaterList from '../../components/TodayWaterList/TodayWaterList';
 // import MonthStatsTable from '../../components/Calendar/Month/Month';
+import AuthContainer from '../../components/AuthContainer/AuthContainer';
+
 import css from './HomePage.module.css';
 
 const HomePage = () => {
   return (
-    <div className={css.background}>
+    <AuthContainer>
       <div className={css.content}>
         {/* <DailyNorma className={css.dialyNorma} /> */}
         <div className={css.bottleSection}>
@@ -15,11 +17,11 @@ const HomePage = () => {
         </div>
 
         <div className={css.statisticsSection}>
-          <TodayWaterList className={css.todayWaterList} />
+          {/* <TodayWaterList className={css.todayWaterList} /> */}
           {/* <MonthStatsTable /> */}
         </div>
       </div>
-    </div>
+    </AuthContainer>
   );
 };
 
--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,8 +1,15 @@
 import Welcome from '../../components/Welcome/Welcome';
-// import css from './WelcomePage.module.css';
+import css from './WelcomePage.module.css';
+import AuthContainer from '../../components/AuthContainer/AuthContainer';
 
 const WelcomePage = () => {
-  return <Welcome />;
+  return (
+    <AuthContainer>
+      <div className={css.welcome}>
+        <Welcome />;
+      </div>
+    </AuthContainer>
+  );
 };
 
 export default WelcomePage;

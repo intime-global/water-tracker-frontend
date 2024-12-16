5ae7d2f309cf322684262a28bc6c2735aa2aa040
AnastasiiaPerevytska <anastasiia.zelenina.1995@gmail.com>
2024-12-17 01:40:46 +0200

    add UserLogo component, add UserLogoModal css, fix Header

--- src/components/SettingsForm/SettingsForm.jsx
+++ src/components/SettingsForm/SettingsForm.jsx
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
 import { editUserInfoThunk } from "../../redux/user/operations.js";
 import { selectIsLoading, selectUser } from "../../redux/user/selectors.js";
-import { notifySuccess } from "../services/notifications.js";
+import { notifySuccess } from "../../services/notifications.js";
 import sprite from "../../icons/sprite.svg";
 import css from "./SettingForm.module.css";
 

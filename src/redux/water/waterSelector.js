926278561376f3b0e661749ada3c948fd2af9431
Dava1991 <134635344+Dava1991@users.noreply.github.com>
2024-12-17 11:59:01 +0200

    today water list code

--- src/redux/water/waterSelector.js
+++ src/redux/water/waterSelector.js
@@ -1,5 +1,7 @@
-export const waterListSelector = (state) => state.water.listItems;
+//export const waterListSelector = (state) => state.water.listItems;
 
-export const waterErrorSelector = (state) => state.water.error;
+//export const waterErrorSelector = (state) => state.water.error;
 
-export const waterIsLoadingSelector = (state) => state.water.isLoading;
+//export const waterIsLoadingSelector = (state) => state.water.isLoading;
+
+export const selectTodayWater = state => state.water.today;

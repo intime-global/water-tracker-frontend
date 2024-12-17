ba790f8177d64db1a718e25a53121cfdad9fc1ec
Denis Chernyak <chernyak.ds@gmail.com>
2024-12-17 22:17:17 +0200

    reset password form and page done

--- src/components/RecoveryForm/RecoveryForm.jsx
+++ src/components/RecoveryForm/RecoveryForm.jsx
@@ -1,54 +1,73 @@
-import { Formik, Form, Field} from 'formik';
-import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { useEffect, useState } from 'react';
+import css from './RecoveryForm.module.css';
 import * as yup from 'yup';
+import { resetPassword } from '../../redux/user/operations';
+import { notifyError, notifySuccess } from '../../services/notifications';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { selectAuthError, selectIsLoading } from '../../redux/user/selectors';
+import sprite from '../../icons/sprite.svg';
+import Loader from '../Loader/Loader';
 
-const SignUpForm = ({ submitFunc, token }) => {
-  const registerSchema = yup.object({
+const registerSchema = yup.object({
     password: yup.string()
       .min(8, 'Password must be at least 8 characters length')
       .max(64).matches(/^[^\s]*$/, 'Password should not contain spaces.')
       .required('Password is required'),
     repeatPassword: yup
       .string()
-      .when([yup.ref('password'), null], 'Passwords must match'),
-  });
-   const [showPassword, setShowPassword] = useState(false);
+      .oneOf([yup.ref('password'), null], 'Passwords must match')
+      .required('Confirm password field must be filled'),
+});
+
+
+const SignUpForm = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate;
+  const [searchParams] = useSearchParams();
+  const token = searchParams.get('token');
+
+  const isLoading = useSelector(selectIsLoading);
+  const isError = useSelector(selectAuthError);
+
+  const [showPassword, setShowPassword] = useState(false);
   const [showRepeatPassword, setShowRepeatPassword] = useState(false);
 
   const toggleShowPassword = () => {
     setShowPassword((prevState) => !prevState);
   };
-
   const toggleShowRepeatPassword = () => {
     setShowRepeatPassword((prevState) => !prevState);
-  }
+  };
+
+
+
+  useEffect(() => {
+    if (isError) {
+      notifyError(isError);
+    }
+  }, [isError]);
+
+
 
   const initialValues = {
     password: '',
     repeatPassword: '',
-    token: token,
+    token,
   };
 
-  function handleSubmit(values, actions) {
-    submitFunc(values.token, values.password);
-    actions.resetForm();
-  }
 
-//   function watchPassFunc(evt) {
-//     const evtTarget = evt.target.closest('.watchPasswordIcon').previousSibling;
-//     if (evtTarget.type === 'password') {
-//       evtTarget.type = 'text';
-//       evt.target.closest('SVG').firstChild.href.baseVal =
-//         sprite + '#eye-opened';
-//       return;
-//     }
-//     if (evtTarget.type === 'text') {
-//       evtTarget.type = 'password';
-//       evt.target.closest('SVG').firstChild.href.baseVal =
-//         sprite + '#eye-closed';
-//       return;
-//     }
-//   }
+  async function handleSubmit(values, actions) {
+    try {
+      await dispatch(resetPassword(values));
+      notifySuccess('Your password has been changed succesfully');
+      actions.resetForm();
+      navigate('/signin');
+    } catch (error) {
+      notifyError(error.message);
+    }
+  }
 
   return (
     <>
@@ -58,33 +77,83 @@ const SignUpForm = ({ submitFunc, token }) => {
           validationSchema={registerSchema}
           onSubmit={handleSubmit}
         >
-          <Form className="form" autoComplete="off">
-            <Field type="hidden" name='token'/>
-            <label className="label" htmlFor="password">
-              Enter your password
-                <Field
-                  className="input"
-                  type="password"
-                  name="password"
-                  placeholder="Password"
-                />
-            </label>
+          {({ errors, touched }) => (
+                    <Form className={css.form} autoComplete="off">
+                      <h2 className={css.title}>Reset password</h2>
+                      <label className={css.label}>
+                        <p className={css.text}>Enter your new password</p>
+                        <div className={css.inputContainer}>
+                          <Field
+                            name="password"
+                            type={showPassword ? 'text' : 'password'}
+                            placeholder="Password"
+                            className={
+                              errors.password && touched.password
+                                ? `${css.input} ${css.inputError}`
+                                : css.input
+                            }
+                          />
+                          <svg
+                            className={css.icon}
+                            onClick={(e) => {
+                              e.preventDefault();
+                              toggleShowPassword();
+                            }}
+                          >
+                            {showPassword ? (
+                              <use href={`${sprite}#icon-eye`} />
+                            ) : (
+                              <use href={`${sprite}#icon-eye-hidden`} />
+                            )}
+                          </svg>
+                        </div>
+                        <ErrorMessage
+                          name="password"
+                          component="div"
+                          className={css.error}
+                        />
+                      </label>
 
-            <label className="label" htmlFor="repeatPassword">
-              Repeat password
-                <Field
-                  className="input"
-                  type="password"
-                  name="repeatPassword"
-                  placeholder="Repeat password"
-                />
-            </label>
-            <br />
+                      <label className={css.label}>
+                        <p className={css.text}>Repeat your new password</p>
+                        <div className={css.inputContainer}>
+                          <Field
+                            name="repeatPassword"
+                            type={showRepeatPassword ? 'text' : 'password'}
+                            placeholder="Repeat password"
+                            className={
+                              errors.repeatPassword && touched.repeatPassword
+                                ? `${css.input} ${css.inputError}`
+                                : css.input
+                            }
+                          />
 
-            <button className="buttonSignUp" type="submit">
-              Reset Password
-            </button>
-          </Form>
+                          <svg
+                            className={css.icon}
+                            onClick={(e) => {
+                              e.preventDefault();
+                              toggleShowRepeatPassword();
+                            }}
+                          >
+                            {showRepeatPassword ? (
+                              <use href={`${sprite}#icon-eye`} />
+                            ) : (
+                              <use href={`${sprite}#icon-eye-hidden`} />
+                            )}
+                          </svg>
+                        </div>
+                        <ErrorMessage
+                          name="repeatPassword"
+                          component="div"
+                          className={css.error}
+                        />
+                      </label>
+
+                      <button type="submit" className={css.button}>
+                        {isLoading ? <Loader /> : 'Reset Password'}
+                      </button>
+                    </Form>
+                  )}
         </Formik>
       </div>
     </>
--- src/pages/EmailResetPage/PasswordResetPage.jsx
+++ src/pages/EmailResetPage/PasswordResetPage.jsx
@@ -1,60 +1,12 @@
-import { useDispatch } from 'react-redux';
-import { Navigate, Link, useSearchParams } from 'react-router-dom';
+import css from './PasswordResetPage.css';
 import RecoveryForm from '../../components/RecoveryForm/RecoveryForm';
-import { resetPassword } from '../../redux/user/operations';
-import { useState } from 'react';
 
 export default function PasswordResetPage () {
-  const { token } = useSearchParams();
-  const [isPassword, setIsPassword] = useState(undefined);
-
-  const dispatch = useDispatch();
-
-  function handleSubmit(password) {
-    dispatch(resetPassword({ token, password }));
-    setIsPassword(password);
-  }
 
   return (
-    <>
-          <div className="wrapper">
-            <div className="formCont">
-              <h2 className="title">Recover Password</h2>
-          <RecoveryForm submitFunc={handleSubmit} token={token} />
-              <div className="link-container">
-                <Link className="link" to="/signin">
-                  Sign In
-                </Link>
-                <Link className="link" to="/signup">
-                  Sign Up
-                </Link>
-              </div>
-            </div>
-            {/* <picture className="bottle">
-              <source
-                srcSet={`${bottleImage_desk_1x} 1x, ${bottleImage_desk_2x} 2x`}
-                media="(min-width: 1440px)"
-                type="image/png"
-              />
-              <source
-                srcSet={`${bottleImage_tab_1x} 1x, ${bottleImage_tab_2x} 2x`}
-                media="(min-width: 768px)"
-                type="image/png"
-              />
-              <source
-                srcSet={`${bottleImage_mob_1x} 1x, ${bottleImage_mob_2x} 2x`}
-                media="(min-width: 320px)"
-                type="image/png"
-              />
-              <img
-                className="bottle"
-                alt="bottle of water"
-                src={bottleImage_mob_1x}
-              />
-            </picture> */}
-          </div>
-      {isPassword && <Navigate to="/signin" />}
-    </>
+    <div className={css.pageContainer}>
+      <RecoveryForm />
+    </div>
   );
 };
 

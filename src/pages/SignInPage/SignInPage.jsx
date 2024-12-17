851463cf2be814bfc1dee78f2f6b93015e1ac547
Olexandr-L45D <litvinenko1978aleks@gmail.com>
2024-12-17 11:27:52 +0200

    add Backgraund to SigninPage (component SigninPageBackgrCard,image)

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,9 +1,16 @@
+// import Auscontainer from '../../components/Auscontainer';
 import SignInForm from '../../components/SignInForm/SignInForm';
+import SigninPageBackgrCard from '../../components/SigninPageBackgrCard/SigninPageImageCard';
 import css from './SignInPage.module.css';
 export default function SugnInPage() {
   return (
+    // <Auscontainer>
     <div className={css.pageContainer}>
-      <SignInForm />
+      <div className={css.pageFlexContainer}>
+        <SignInForm />
+        <SigninPageBackgrCard />
+      </div>
     </div>
+    // <Auscontainer />
   );
 }

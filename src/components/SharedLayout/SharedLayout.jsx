8075aebeee7e9fe60436e5bab338da9e381eedee
Nadiia Yuce <soyka9616@gmail.com>
2024-12-18 04:37:06 +0100

    delete SharedLayout styles, fix header, FIX welcomePage #83

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,11 +1,10 @@
-import css from './SharedLayout.module.css';
 import Header from '../Header/Header.jsx';
 
 export default function SharedLayout({ children }) {
   return (
-    <div className={css.layoutContainer}>
+    <>
       <Header />
       <main>{children}</main>
-    </div>
+    </>
   );
 }
--- src/components/WhyDrinkWater/WhyDrinkWater.jsx
+++ src/components/WhyDrinkWater/WhyDrinkWater.jsx
@@ -1,55 +1,33 @@
 import css from './WhyDrinkWater.module.css';
-import sprite from '../../icons/sprite.svg';
 
 const WhyDrinkWater = () => {
   return (
     <div className={css.reasons}>
       <h3 className={css.title3}>Why drink water</h3>
-      <ul className={css.listReasons}>
-        <li>
-          <svg className={css.benefitsIcon}>
-            <use href={`${sprite}#icon-Ellipse-7`}></use>
-          </svg>
+      <ul className={css.reasonsList}>
+        <li className={css.listItem}>
           <p className={css.listText}>Supply of nutrients to all organs</p>
         </li>
-        <li>
-          <svg className={css.benefitsIcon}>
-            <use href={`${sprite}#icon-Ellipse-7`}></use>
-          </svg>
+        <li className={css.listItem}>
           <p className={css.listText}>Providing oxygen to the lungs</p>
         </li>
-        <li>
-          <svg className={css.benefitsIcon}>
-            <use href={`${sprite}#icon-Ellipse-7`}></use>
-          </svg>
+        <li className={css.listItem}>
           <p className={css.listText}>Maintaining the work of the heart</p>
         </li>
-        <li>
-          <svg className={css.benefitsIcon}>
-            <use href={`${sprite}#icon-Ellipse-7`}></use>
-          </svg>
+        <li className={css.listItem}>
           <p className={css.listText}>Release of processed substances</p>
         </li>
-        <li>
-          <svg className={css.benefitsIcon}>
-            <use href={`${sprite}#icon-Ellipse-7`}></use>
-          </svg>
+        <li className={css.listItem}>
           <p className={css.listText}>
             Ensuring the stability of the internal environment
           </p>
         </li>
-        <li>
-          <svg className={css.benefitsIcon}>
-            <use href={`${sprite}#icon-Ellipse-7`}></use>
-          </svg>
+        <li className={css.listItem}>
           <p className={css.listText}>
             Maintaining within the normal temperature
           </p>
         </li>
-        <li>
-          <svg className={css.benefitsIcon}>
-            <use href={`${sprite}#icon-Ellipse-7`}></use>
-          </svg>
+        <li className={css.listItem}>
           <p className={css.listText}>
             Maintaining an immune system capable of resisting disease
           </p>

8e59f95440de4a95d02be6006cef5feec76c4a45
Nadiia Yuce <soyka9616@gmail.com>
2024-12-18 15:05:54 +0100

    return component Svg, delete svg icon which is not in use

---
+++ src/components/Icons/icons.jsx
@@ -0,0 +1,9 @@
+import Icons from '../../icons/sprite.svg';
+
+export const Svg = ({ id, width, height }) => {
+  return (
+    <svg width={width} height={height}>
+      <use href={Icons + id}></use>
+    </svg>
+  );
+};

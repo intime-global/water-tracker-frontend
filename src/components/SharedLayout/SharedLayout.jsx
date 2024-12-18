7e58c0408c007b1bb0440929fe203dce73252fe3
Nadiia Yuce <soyka9616@gmail.com>
2024-12-18 12:44:13 +0100

    fix scroll on Welcome page

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,10 +1,11 @@
+// import css from './SharedLayout.module.css';
 import Header from '../Header/Header.jsx';
 
 export default function SharedLayout({ children }) {
   return (
-    <>
+    <div>
       <Header />
       <main>{children}</main>
-    </>
+    </div>
   );
 }

4c118c3098d6697d13f0cab133832e438fe81e5a
Victoria <vika96karnauhova@gmail.com>
2024-12-20 15:16:24 +0200

    delete AuthContainer,fix background

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -8,22 +8,26 @@ import css from './HomePage.module.css';
 
 const HomePage = () => {
   return (
-   <>
+    <>
       {/* Розмітка для перевірки */}
-      <div className={css.home}>
-        <div className={css.bottleSection}>
-          <div className={css.dailyNorma}><DailyNorma/></div>
-          <div className={css.bottle}></div>
-          <div>WaterRatioPanel</div>
-        </div>
+      <div className={css.background}>
+        <div className={css.home}>
+          <div className={css.bottleSection}>
+            <div className={css.dailyNorma}>DailyNorma</div>
+            {/* <DailyNorma className={css.dailyNorma} /> */}
+            <div className={css.bottle}></div>
+            <div>WaterRatioPanel</div>
+          </div>
 
-        <div className={css.statisticsSection}>
-          <div className={css.todayWaterList}>TodayWaterList</div>
-          <div>MonthStatsTable</div>
+          <div className={css.statisticsSection}>
+            <div className={css.todayWaterList}>TodayWaterList</div>
+            <div>MonthStatsTable</div>
+          </div>
         </div>
       </div>
-
-      {/* <div className={css.home}>
+      {/*
+      <div className={css.background}>
+      <div className={css.home}>
         <div className={css.bottleSection}>
           <DailyNorma className={css.dialyNorma} />
           <div className={css.bottle}></div>
@@ -34,7 +38,8 @@ const HomePage = () => {
           <TodayWaterList className={css.todayWaterList} />
           <MonthStatsTable />
         </div>
-      </div> */}
+      </div>
+      </div>*/}
     </>
   );
 };

96876073336523bcb8d555e9c5ee8d47e97c250b
Victoria <vika96karnauhova@gmail.com>
2024-12-22 00:20:25 +0200

    fix background

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -8,38 +8,36 @@ import css from './HomePage.module.css';
 
 const HomePage = () => {
   return (
-    <>
-      {/* Розмітка для перевірки */}
-      <div className={css.background}>
-        <div className={css.home}>
-          <div className={css.bottleSection}>
-            <DailyNorma className={css.dailyNorma} />
-            <div className={css.bottle}></div>
-            <div>WaterRatioPanel</div>
-          </div>
-
-          <div className={css.statisticsSection}>
-            <div className={css.todayWaterList}>TodayWaterList</div>
-            <div>MonthStatsTable</div>
-          </div>
-        </div>
-      </div>
-      {/*
-      <div className={css.background}>
+    <div className={css.background}>
       <div className={css.home}>
         <div className={css.bottleSection}>
+          {/* <div className={css.dailyNorma}>dailyNorma</div> */}
           <DailyNorma className={css.dailyNorma} />
-          <div className={css.bottle}></div>
-          <WaterRatioPanel />
+          <div>WaterRatioPanel</div>
         </div>
 
         <div className={css.statisticsSection}>
-          <TodayWaterList className={css.todayWaterList} />
-          <MonthStatsTable />
+          <div className={css.todayWaterList}>TodayWaterList</div>
+          <div>MonthStatsTable</div>
         </div>
       </div>
-      </div>*/}
-    </>
+    </div>
+
+    // {/*
+    // <div className={css.background}>
+    // <div className={css.home}>
+    //   <div className={css.bottleSection}>
+    //     <DailyNorma className={css.dailyNorma} />
+    //     <div className={css.bottle}></div>
+    //     <WaterRatioPanel />
+    //   </div>
+
+    //   <div className={css.statisticsSection}>
+    //     <TodayWaterList className={css.todayWaterList} />
+    //     <MonthStatsTable />
+    //   </div>
+    // </div>
+    // </div>*/}
   );
 };
 

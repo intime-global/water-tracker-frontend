1afc53e318abf9d6a24cb6786da0a1f517125ce7
Hanna Lakus <annalakus@ukr.net>
2024-12-22 01:06:20 +0200

    add button style

--- src/components/GoogleLoginButton/GoogleLoginButton.jsx
+++ src/components/GoogleLoginButton/GoogleLoginButton.jsx
@@ -1,20 +1,25 @@
 // import { useDispatch} from "react-redux"
 // import { getOauthUrl } from "../../redux/user/operations";
-import { notifyError } from "../../services/notifications";
-import css from './GoogleLoginButton.module.css'
-import axios from "axios";
+import { notifyError } from '../../services/notifications';
+import css from './GoogleLoginButton.module.css';
+import axios from 'axios';
+
+import { FcGoogle } from 'react-icons/fc';
+
 // import { selectIsLoading } from "../../redux/user/selectors";
 
 const GoogleLoginButton = () => {
-    // const dispatch = useDispatch();
-    // const isLoading = useSelector(selectIsLoading);
+  // const dispatch = useDispatch();
+  // const isLoading = useSelector(selectIsLoading);
 
-    const handleGoogleLogin = async () => {
-        try {
-      const { data: {data: {url}} } = await axios.get(
-        'http://localhost:3000/auth/get-oauth-url'
-      );
-            console.log('URL:', url);
+  const handleGoogleLogin = async () => {
+    try {
+      const {
+        data: {
+          data: { url },
+        },
+      } = await axios.get('http://localhost:3000/auth/get-oauth-url');
+      console.log('URL:', url);
       window.location.href = url;
     } catch (error) {
       console.error('Error:', error);
@@ -22,14 +27,14 @@ const GoogleLoginButton = () => {
     }
   };
 
-    return (
-        <>
-            <button onClick={handleGoogleLogin} className={css.button}>
-                Login with Google
-            </button>
-        </>
-    );
-
+  return (
+    <>
+      <button onClick={handleGoogleLogin} className={css.button}>
+        <span>Login with Google</span>
+        <FcGoogle size={25} />
+      </button>
+    </>
+  );
 };
 
 export default GoogleLoginButton;

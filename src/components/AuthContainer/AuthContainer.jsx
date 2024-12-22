39ec57271ddf0c79d7b5988ed05ed33ce156a24f
ustym-karpiuk <karpyuk0502@gmail.com>
2024-12-22 06:37:25 +0100

    Update AuthContainer
    Fix css  in background and add container for authPages

--- src/components/AuthContainer/AuthContainer.jsx
+++ src/components/AuthContainer/AuthContainer.jsx
@@ -1,5 +1,9 @@
 import css from './AuthContainer.module.css';
 
-export default function AuthContainer({ children}) {
-  return <div className={css.background}>{children}</div>;
+export default function AuthContainer({ children }) {
+  return (
+    <div className={css.background}>
+      <div className={css.container}>{children}</div>
+    </div>
+  );
 }
--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,11 +1,9 @@
 import AuthContainer from '../../components/AuthContainer/AuthContainer';
 import SignInForm from '../../components/SignInForm/SignInForm';
-// import SigninPageBackgrCard from '../../components/SigninPageBackgrCard/SigninPageImageCard';
-// import css from './SignInPage.module.css';
 export default function SugnInPage() {
   return (
     <AuthContainer>
-            <SignInForm />
+      <SignInForm />
     </AuthContainer>
   );
 }

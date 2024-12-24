cea14cc8b9e970c5b302c58b2765feb55e7831d3
Denis Chernyak <chernyak.ds@gmail.com>
2024-12-24 03:09:34 +0200

    dd

--- src/services/axios.config.js
+++ src/services/axios.config.js
@@ -36,6 +36,9 @@ export const setupInterceptors = (store) => {
             'Headers: ',
             axiosInstance.defaults.headers.common.Authorization,
           );
+          originalRequest.headers[
+            'Authorization'
+          ] = `Bearer ${peremenaya.data.accessToken}`;
           return axiosInstance(originalRequest); // Retry the original request with the new access token.
         } catch (refreshError) {
           // Handle refresh token errors by clearing stored tokens and redirecting to the login page.

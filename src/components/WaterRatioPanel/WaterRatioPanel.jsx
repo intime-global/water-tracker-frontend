39a4562d8e3da8b70191d08d55f657878132fcbc
Serhii Reshetniak <reshet@lozovaya.org.ua>
2024-12-25 00:42:35 +0200

    Fix percentage

--- src/components/WaterRatioPanel/WaterRatioPanel.jsx
+++ src/components/WaterRatioPanel/WaterRatioPanel.jsx
@@ -11,12 +11,14 @@ const WaterRatioPanel = () => {
   const initMonth = new Date().getMonth() + 1;
   const initYear = new Date().getFullYear();
 
-  const percent = useSelector(selectMonthWater);
+  const monthStats = useSelector(selectMonthWater);
 
   // Get percentage from state instead of calculating
-  const { percentage } = percent.find(
+  const todaysStats = monthStats.find(
     (o) => o.day == initDay && o.month == initMonth && o.year == initYear,
   );
+
+  const percentage = todaysStats?.percentage || 0;
 
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [initialAmount, setInitialAmount] = useState(0);
